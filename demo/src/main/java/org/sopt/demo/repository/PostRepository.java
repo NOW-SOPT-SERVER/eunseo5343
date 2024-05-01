c82a92cac3a0cde5d2daed7157bf3b8bfd82c705
eunseo5343 <130284467+eunseo5343@users.noreply.github.com>
2024-05-01 21:45:29 +0900

    3차 과제 코드

---
+++ demo/src/main/java/org/sopt/demo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package org.sopt.demo.repository;
+
+import org.sopt.demo.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+
+}
---
+++ demo/src/main/java/org/sopt/demo/service/dto/PostCreateRequest.java
@@ -0,0 +1,7 @@
+package org.sopt.demo.service.dto;
+
+public record PostCreateRequest(
+        String title,
+        String content
+
+) {}
